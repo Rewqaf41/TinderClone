3317acd58ab456611260e44abac1df4689e8d46e
Rewqaf41 <danyburdin@gmail.com>
2025-10-30 21:33:59 +0300

    fix/progress bar fixed

--- src/shared/ui/Progress/ProgressBar.tsx
+++ src/shared/ui/Progress/ProgressBar.tsx
@@ -7,7 +7,7 @@ export const ProgressBar = ({ currentStep, totalSteps }: ProgressBarProps) => {
 	const progress = (currentStep / totalSteps) * 100
 
 	return (
-		<div className='fixed top-0 right-0 left-0 z-50 h-2.5 bg-gray-900'>
+		<div className='fixed top-0 left-1/2 z-50 h-2.5 w-full max-w-(--max-mobile-width) -translate-x-1/2 bg-gray-900'>
 			<div
 				className='h-full bg-linear-to-r from-orange-500 via-pink-500 to-pink-600 transition-all duration-300 ease-out'
 				style={{ width: `${progress}%` }}
