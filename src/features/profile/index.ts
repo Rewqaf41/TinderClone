130b285e02ec5c6120df3bf8c73f5f1c9f8070fb
Rewqaf <115074036+Rewqaf41@users.noreply.github.com>
2025-10-30 16:33:16 +0300

    feat: created modal for adding about me info (#4)

--- src/features/profile/index.ts
+++ src/features/profile/index.ts
@@ -1,4 +1,5 @@
 export { ActionButtons } from "./ui/ActionButtons"
+export { MoreAboutMeModal } from "./ui/MoreAboutMeModal"
 export { ProfileInfo } from "./ui/ProfileInfo"
 export { TinderPlatinumSlider } from "./ui/TinderPlatinumSlider"
 
---
+++ src/features/profile/model/moreAboutMeModal.data.ts
@@ -0,0 +1,30 @@
+export const personalityTypes = [
+	'INTJ',
+	'INTP',
+	'ENTJ',
+	'ENTP',
+	'INFJ',
+	'INFP',
+	'ENFJ',
+	'ENFP',
+	'ISTJ',
+	'ISFJ',
+	'ESTJ',
+	'ESFJ',
+	'ISTP',
+	'ISFP',
+	'ESTP',
+	'ESFP'
+]
+
+export const communicationStyles = [
+	'I stay on WhatsApp all day',
+	'Big time texter',
+	'Phone caller',
+	'Video chatter',
+	"I'm slow to answer on WhatsApp",
+	'Bad texter',
+	'Better in person'
+]
+
+export const loveLanguages = ['Thoughtful gestures', 'Presents', 'Touch', 'Compliments', 'Time together']
