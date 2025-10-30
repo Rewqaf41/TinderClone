25f3270a08b7dc41183f74e7148cdc82e469501a
Rewqaf <115074036+Rewqaf41@users.noreply.github.com>
2025-10-30 17:03:26 +0300

    Feat/husky (#5)
    * feat: adding husky in project for linting
    
    * fix: fixed all linter issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 3000
-  }
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src')
+		}
+	},
+	server: {
+		port: 3000
+	}
 })
